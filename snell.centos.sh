79272240c9e94d2c4c77e0d3006de687770d0ea2
wbch <51674759+wbch@users.noreply.github.com>
2022-10-31 12:45:31 +0800

    Update snell.centos.sh

--- snell.centos.sh
+++ snell.centos.sh
@@ -22,7 +22,7 @@ if [ -f ${CONF} ]; then
   mkdir /etc/snell/
   echo "Generating new config..."
   echo "[snell-server]" >>${CONF}
-  echo "listen = 0.0.0.0:13353" >>${CONF}
+  echo "listen = 0.0.0.0:6333" >>${CONF}
   echo "psk = ${PSK}" >>${CONF}
   echo "ipv6 = false" >>${CONF}
 fi
